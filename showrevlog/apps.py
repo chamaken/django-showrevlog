a5da3b415c6f5d0d1387e16fb0486006dbb1b806
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2018-01-19 18:12:38 +0900

    admin: update message

--- showrevlog/apps.py
+++ showrevlog/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
 
 class ShowRevLogConfig(AppConfig):
     name = 'showrevlog'
+    verbose_name = _('Show logfile in reverse order')
