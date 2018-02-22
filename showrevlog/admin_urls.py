082b0aeb60a079e4fb4fbb9b9e396f7b242863d0
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2018-02-22 10:58:18 +0900

    urls: add app_name
    To supress deprecated warning, see:
    
      https://docs.djangoproject.com/en/dev/topics/http/urls/

--- showrevlog/admin_urls.py
+++ showrevlog/admin_urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from .views import *
 
+
+app_name = 'showrevlog'
+
 urlpatterns = [
     url(r'^$', index, {'template_name': 'showrevlog/files.html'}, name='index'),
     url(r'^(?P<logfile_id>\d+)/$', show, {'template_name': 'showrevlog/show.html'}, name='show'),
